cfe72bf10b43097170ca66f55fd8d69c9f99203b
Minakshee Bari <minaksheebari22@gmail.com>
2023-11-07 20:59:35 +0530

    Adding Day 3 file of Insert, Update & Delete in table

---
+++ SQL CLASS/Day3.sql
@@ -0,0 +1,37 @@
+-- Day 3
+
+-- 1. Inserting records into table by specifying column names
+INSERT INTO employee(empid,firstname,lastname,designation,emailid,address,city) 
+VALUES(101,'Minakshee','Bari','Asst.Prof','minu@gmail.com','MH','Jalgaon');
+
+-- to see all the columns in table
+SELECT * FROM employee;
+
+-- 2. Inserting records into table without specifying column names
+INSERT INTO employee VALUES(102,'Kishor','Mali','Technician','kish@gmail.com','MH','Jalgaon');
+
+-- 3. Inserting Multiple records into table
+INSERT INTO employee VALUES
+(103,'Yash','Jain','Assistant','yash@gmail.com','MH','Jalgaon'),
+(104,'Mitali','Malva','HR','mit@gmail.com','MH','Jalgaon'),
+(105,'Nisha','Kapoor','CA','nish@gmail.com','MH','Jalgaon'),
+(106,'Kush','Gupta','Salesman','kush@gmail.com','MH','Jalgaon');
+
+
+-- Updating already existing records
+UPDATE employee
+SET address = 'MP'
+WHERE empid=102;
+
+-- DELETE particular row from table
+DELETE FROM employee where empid=103;
+
+-- DELETE all rows from table
+DELETE FROM employee;
+
+
+
+
+
+
+
