0ebe5495e8159897853fcbfe74e83bf8ad154640
Minakshee Bari <minaksheebari22@gmail.com>
2023-11-21 20:58:08 +0530

    Adding day 6 file

---
+++ SQL CLASS/Day6.sql
@@ -0,0 +1,30 @@
+select * from student;
+
+#Alter table - DDL Command
+alter table student add column city varchar(20) after marks;
+Update student SET city = "Jalgaon" where address ="MH";
+
+update student
+SET city = 
+  CASE 
+    WHEN address = 'MH' THEN 'Jalgaon'
+    WHEN address = 'MP' THEN 'Bhopal'
+    ELSE city
+  END;
+  
+  update student set city="Dhule" where address="Jalgaon" OR city="Indore";
+
+#order by
+select * from student order by age asc;
+select * from student order by age desc;
+
+# If you want to sort based on multiple columns, 
+# It will give priority to first mentioned column i.e. sort desc by age
+select * from student order by age,marks desc;
+
+
+#group by
+
+#how to delete a col, 
+#how to rename a col name, 
+#how to add a new constraint in an existing col
